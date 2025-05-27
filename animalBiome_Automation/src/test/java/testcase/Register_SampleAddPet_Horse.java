4fb430ed5c681cce4ae9f5ec7cfc1811f48a99cc
banoj <banoj@banoj-sa.stratapps.com>
2025-05-27 15:34:43 +0530

    Eliminated lines of code for user_Others add pet scenario

--- animalBiome_Automation/src/test/java/testcase/Register_SampleAddPet_Horse.java
+++ animalBiome_Automation/src/test/java/testcase/Register_SampleAddPet_Horse.java
@@ -52,13 +52,9 @@ public void AddingHorsePetDetails() throws InterruptedException, IOException {
 
 	ExcelUtils.loadExcelFile(excelPath, sheetName);
 
-	String speciesSelected = "User_Horse";
 	String newPetName = base.ExcelUtils.UniquePetName(prop.getProperty("N_Enter_Horse_Name"));
 
 	driver.findElement(By.xpath(prop.getProperty("N_Horse_Name"))).sendKeys(newPetName);
-
-	ExcelUtils.addPetNameToSpeciesColumn(speciesSelected, newPetName);
-	
 	Thread.sleep(1500);
 	driver.findElement(By.xpath(prop.getProperty("N_Age_Years"))).sendKeys(prop.getProperty("N_enter_Age"));
 	Thread.sleep(1500);
