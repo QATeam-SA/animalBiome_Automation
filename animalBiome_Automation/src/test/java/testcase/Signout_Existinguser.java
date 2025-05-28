3e916e82b4ea3c8df4ba2324fc44a7ac128015d3
banoj <banoj@banoj-sa.stratapps.com>
2025-05-28 16:26:27 +0530

    Path for the image uploads has been changed

--- animalBiome_Automation/src/test/java/testcase/Signout_Existinguser.java
+++ animalBiome_Automation/src/test/java/testcase/Signout_Existinguser.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import base.Instance;
@@ -16,14 +15,14 @@ public class Signout_Existinguser {
 	WebDriver driver = Instance.getInstance();
 	Properties prop = PropertiesFile.readPropertyFile("Signout_Existinguser.properties");
 	
-	@Test(priority=28, enabled=true)
+	@Test(enabled=true)
 	public void signout() throws InterruptedException {
-		driver.findElement(By.xpath(prop.getProperty("j_My_Account"))).click();
-		Thread.sleep(5000);
-		driver.findElement(By.xpath(prop.getProperty("j_SignOut"))).click();
-		Thread.sleep(5000);
-		System.out.println("signedout successfully");
-		logger.info("Existing user is sucessfully Successfully SignedOut");
-		throw new SkipException("Skipping this test on purpose.");
+	    Thread.sleep(4000);
+	    driver.findElement(By.xpath(prop.getProperty("j_My_Account"))).click();
+	    Thread.sleep(5000);
+	    driver.findElement(By.xpath(prop.getProperty("j_SignOut"))).click();
+	    Thread.sleep(5000);
+	    System.out.println("signedout successfully");
+	    logger.info("Existing user is sucessfully Successfully SignedOut");
 	}
 }
--- animalBiome_Automation/src/test/java/testcase/Userprofile_pswd.java
+++ animalBiome_Automation/src/test/java/testcase/Userprofile_pswd.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import base.Instance;
@@ -17,8 +18,15 @@ public class Userprofile_pswd {
 	WebDriver driver = Instance.getInstance();
 	Properties prop = PropertiesFile.readPropertyFile("Userprofile_pswd.properties");
 	
-	@Test(priority=15,enabled=true)
+	@Test(enabled=true)
 	public void ChangingTheUserProfilePassword() throws InterruptedException {
+		    // APPROACH 1: Conditional Skip - Set to true to skip, false to run
+		    boolean skipThisTest = true; // Change to false when you want to run this test
+		    if (skipThisTest) {
+		        throw new SkipException("Skipping change the userprofile password test - Feature temporarily disabled");
+		    }
+		    
+		    // ALL YOUR EXISTING CODE STAYS HERE - NO UNREACHABLE CODE ERROR
 		Thread.sleep(4000);
 		JavascriptExecutor jse = (JavascriptExecutor) driver;
 		driver.findElement(By.xpath(prop.getProperty("j_myacnt_userprofile"))).click();
