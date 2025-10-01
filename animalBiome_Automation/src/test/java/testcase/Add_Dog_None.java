e92226047da1a83217be6d709a9c197b597143b5
banoj <banoj@banoj-sa.stratapps.com>
2025-10-01 15:05:22 +0530

    Committed lines of code for Add_Dog_None, pet_name_counter.

--- animalBiome_Automation/src/test/java/testcase/Add_Dog_None.java
+++ animalBiome_Automation/src/test/java/testcase/Add_Dog_None.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
@@ -15,14 +16,14 @@ import base.Instance;
 import base.PropertiesFile;
 
 public class Add_Dog_None {
-	
+
 	Logger logger = LogManager.getLogger(Add_Dog_None.class);
 	WebDriver driver = Instance.getInstance();
 	Properties prop = PropertiesFile.readPropertyFile("Add_Dog_None.properties");
 
-	@Test(priority = 5, enabled=true)
+	@Test(priority = 5, enabled = true)
 	public void adddog() throws Exception {
-		
+
 		// ------- MY PET---------
 
 		JavascriptExecutor js = MydogDetails();
@@ -30,16 +31,20 @@ public class Add_Dog_None {
 		// ----------PET HEALTH---------
 
 		PetHealthdetails(js);
-				
+
 	}
-	
-   private void PetHealthdetails(JavascriptExecutor js) throws InterruptedException {
-		
-		Thread.sleep(1500);
+
+	private void PetHealthdetails(JavascriptExecutor js) throws InterruptedException {
+
+		logger.info("****Started adding user dog health details****");
+		Thread.sleep(2000);
 		driver.findElement(By.xpath(prop.getProperty("G_Antibiotics"))).click();
-		Thread.sleep(1500);
-		driver.findElement(By.id(prop.getProperty("G_Bodyconditions"))).click();
+		Thread.sleep(2000);
+		WebElement bodycond = driver.findElement(By.id(prop.getProperty("G_Bodyconditions")));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", bodycond);
 		Thread.sleep(1000);
+		bodycond.click();
+		Thread.sleep(3000);
 		js.executeScript("window.scrollBy(0,1100)");
 		Thread.sleep(1500);
 		driver.findElement(By.xpath(prop.getProperty("G_Medications"))).click();
@@ -50,35 +55,46 @@ public class Add_Dog_None {
 		js.executeScript("window.scrollBy(0,300)");
 		Thread.sleep(1500);
 		driver.findElement(By.xpath(prop.getProperty("G_Physicalconditions"))).click();
+		Thread.sleep(1500);
 		driver.findElement(By.xpath(prop.getProperty("G_Diet"))).click();
 		js.executeScript("window.scrollBy(0,100)");
 		Thread.sleep(1500);
 		driver.findElement(By.xpath(prop.getProperty("G_Dietoptions"))).click();
-		js.executeScript("window.scrollBy(0,100)");
+		js.executeScript("window.scrollBy(0,200)");
 		Thread.sleep(2000);
-		driver.findElement(By.xpath(prop.getProperty("G_Symptoms"))).click();
+		WebElement sympt = driver.findElement(By.xpath(prop.getProperty("G_Symptoms")));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", sympt);
+		Thread.sleep(1000);
+		sympt.click();
 		Thread.sleep(1500);
 		driver.findElement(By.xpath(prop.getProperty("G_PetHealth_Nextbutton"))).click();
 		Thread.sleep(3000);
 		js.executeScript("window.scrollBy(0,100)");
 		Thread.sleep(3000);
-		driver.findElement(By.xpath(prop.getProperty("G_Complete"))).click();
+		WebElement completebtn = driver.findElement(By.xpath(prop.getProperty("G_Complete")));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", completebtn);
+		Thread.sleep(1000);
+		completebtn.click();
 		System.out.println("Pet added Successfully");
-		Thread.sleep(3000);
-		driver.findElement(By.xpath(prop.getProperty("G_View_PetPortal"))).click();
+		Thread.sleep(3000);		
+		WebElement veiwpetbtn = driver.findElement(By.xpath(prop.getProperty("G_View_PetPortal")));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", veiwpetbtn);
+		Thread.sleep(1000);
+		veiwpetbtn.click();
 		Thread.sleep(3000);
 		System.out.println("Return to Pet Portal Successfully");
-	
+
 	}
-		
+
 	private JavascriptExecutor MydogDetails() throws InterruptedException, Exception {
 
 		Thread.sleep(10000);
 		driver.findElement(By.xpath(prop.getProperty("G_AddPet"))).click();
-		Thread.sleep(5000);
-		driver.findElement(By.xpath(prop.getProperty("G_DogButton"))).click();
 		Thread.sleep(3000);
-		JavascriptExecutor js = (JavascriptExecutor)driver;
+		WebElement element = driver.findElement(By.xpath(prop.getProperty("G_DogButton")));
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+		Thread.sleep(3000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,600)");
 		Thread.sleep(2000);
 		driver.findElement(By.xpath(prop.getProperty("G_SelectBreed"))).click();
@@ -100,27 +116,30 @@ public class Add_Dog_None {
 		driver.findElement(By.name(prop.getProperty("G_Dogyears"))).sendKeys("3");
 		driver.findElement(By.name(prop.getProperty("G_Dogmonths"))).sendKeys("5");
 		Thread.sleep(2000);
-		driver.findElement(By.xpath(prop.getProperty("G_Dogsex"))).click();
-		js.executeScript("window.scrollBy(0,300)");
-		Thread.sleep(2000);
+		WebElement ele = driver.findElement(By.xpath(prop.getProperty("G_Dogsex")));
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", ele);
+		Thread.sleep(3000);
 		driver.findElement(By.name(prop.getProperty("G_Dogweight"))).sendKeys("56");
 		Thread.sleep(1000);
 		driver.findElement(By.xpath(prop.getProperty("G_weightunits"))).click();
 		Thread.sleep(1500);
-		driver.findElement(By.xpath(prop.getProperty("G_Doggender"))).click();
-		js.executeScript("window.scrollBy(0,100)");
+		WebElement gender = driver.findElement(By.xpath(prop.getProperty("G_Doggender")));
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", gender);
 		Thread.sleep(1500);
 		driver.findElement(By.xpath(prop.getProperty("G_Street"))).sendKeys("Street");
+		Thread.sleep(1000);
 		driver.findElement(By.xpath(prop.getProperty("G_City"))).sendKeys("City");
+		Thread.sleep(1000);
 		driver.findElement(By.xpath(prop.getProperty("G_State"))).sendKeys("State");
+		Thread.sleep(1000);
 		driver.findElement(By.xpath(prop.getProperty("G_PostalCode"))).sendKeys("123456789");
-		Select s=new Select(driver.findElement(By.xpath(prop.getProperty("G_Country"))));
+		Thread.sleep(1000);
+		Select s = new Select(driver.findElement(By.xpath(prop.getProperty("G_Country"))));
 		s.selectByVisibleText("Australia");
 		Thread.sleep(1500);
 		driver.findElement(By.xpath(prop.getProperty("G_AddPet_Nextbutton"))).click();
 		return js;
-		
+
 	}
-	
-	
+
 }
